63e5c48b5e535eed4839a5d1f9b9568602b21cd0
Joao Antonio <joaoantonios74@gmail.com>
2025-03-01 10:20:56 -0300

    vencedor e ajusta a simulação de pulos com tempo aleatório

--- corrida.py
+++ corrida.py
@@ -4,21 +4,25 @@ import random
 
 #define uma classe Sapo
 class Sapo(threading.Thread):
+    vencedor = None  # Variável compartilhada para armazenar o vencedor
+
     def __init__(self, ident):
         threading.Thread.__init__(self)
         self.ident = ident
         self.pulos = 0
         self.posicao = 0
         self.pulo = 0
-        self.velocidade = random.uniform(0.5, 2.0)  # Define a velocidade como um número aleatório entre 0.5 e 2.0 segundos
 
     def run(self):
         while self.posicao < 50:  # Altera a distância da corrida para 50 metros
             self.pulo = random.randint(1, 5)
             self.posicao += self.pulo
             self.pulos += 1
-            print("Sapo %d pulou %d cm e percorreu %d cm" % (self.ident, self.pulo, self.posicao))
-            time.sleep(self.velocidade)  # Usa a velocidade para determinar o tempo de espera entre os pulos
+            print("Sapo %d pulou %d metros e percorreu %d metros" % (self.ident, self.pulo, self.posicao))
+            time.sleep(random.uniform(0.1, 0.5))  # Dorme um tempo aleatório entre 0.1s e 0.5s para simular diferenças de velocidade
+            if self.posicao >= 50 and Sapo.vencedor is None:
+                Sapo.vencedor = self.ident
+                print("Sapo %d é o vencedor!" % self.ident)
 
 def corrida():
     sapos = [Sapo(i) for i in range(10)]
